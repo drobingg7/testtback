a27bc9f26e51d123e6f38336e62529dcae087bc1
drobingg7 <ikanis67@outlook.com>
2026-07-11 21:28:24 +0300

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-// Создаём файл на рабочем столе
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-var desktop = fso.GetSpecialFolder(0); // 0 = Desktop
-var filePath = desktop + "\\proof.txt";
-var file = fso.CreateTextFile(filePath, true);
-file.WriteLine("Скрипт выполнился!");
-file.Close();
